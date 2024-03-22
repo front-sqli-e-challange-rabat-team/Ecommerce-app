1625e481cfb069ae6fca98776a7a63cc51e3e9cf
Mohamed Malainine Mohammed Chakour <mmohammedchakour@smb.sqli.com>
2024-03-22 15:40:34 +0000

    add : Card component beta

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,32 @@
-import { TitleUi } from '@repo/ui';
+import ProductCard from "./components/product-card";
 
 function App() {
+  const products = [{
+    id: 1,
+    imageUrl: "https://img.freepik.com/free-photo/new-pair-white-sneakers-isolated-white_93675-130969.jpg",
+    name: "Gucci duffle bag",
+    discountRate: 50,
+    originalPrice: 1160,
+    rating: 4.5,
+    reviewCount: 65,
+  }];
+
   return (
     <>
-      <div className='bg-red-300 text-center'>anas jaidi</div>
-      <TitleUi />
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {products.map((product : any) => (
+            <ProductCard
+              imageUrl={product.imageUrl}
+              name={product.name}
+              discountRate={product.discountRate}
+              originalPrice={product.originalPrice}
+              rating={product.rating}
+              reviewCount={product.reviewCount}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 import config from '@repo/tailwind-config/tailwind.config';
 export default {
+  ...config,
   content: [
     'src/**/*.{js,jsx,ts,tsx}',
     '../../packages/ui/src/**/*.{js,jsx,ts,tsx}',
   ],
-  ...config,
+  plugins: [...config.plugins, require("daisyui")],
+  daisyui: {
+    ...config.daisyui,
+    themes:["dark", "nord"],
+    base: true,
+    styled: true,
+    utils: true,
+    prefix:'',
+    logs:true,
+    themeRoot: ":root"
+  },
 };
