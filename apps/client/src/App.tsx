31f070bfe31bf1d343390da763e41da6bacefd61
Elkarni Bachar <diamobachar06@gmail.com>
2024-03-24 15:50:55 +0000

    add daisyui config

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,6 @@
-import { TitleUi } from '@repo/ui';
-
 function App() {
   return (
-    <>
-      <div className='bg-red-300 text-center'>anas jaidi</div>
-      <TitleUi />
-    </>
+    <div></div>
   );
 }
 
