67c3dc1b2654cec4da9a881ac4bee05e656bfe2c
Mohamed Malainine Mohammed Chakour <mmohammedchakour@smb.sqli.com>
2024-03-24 01:34:12 +0000

    Enhance product cards, add rating feature, and implement full-width carousel

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,31 +1,12 @@
-import ProductCard from "./components/product-card";
+import Carousel from "./components/carousel";
 
 function App() {
-  const products = [{
-    id: 1,
-    imageUrl: "https://img.freepik.com/free-photo/new-pair-white-sneakers-isolated-white_93675-130969.jpg",
-    name: "Gucci duffle bag",
-    discountRate: 50,
-    originalPrice: 1160,
-    rating: 4.5,
-    reviewCount: 65,
-  }];
+  
 
   return (
     <>
       <div className="container mx-auto p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {products.map((product : any) => (
-            <ProductCard
-              imageUrl={product.imageUrl}
-              name={product.name}
-              discountRate={product.discountRate}
-              originalPrice={product.originalPrice}
-              rating={product.rating}
-              reviewCount={product.reviewCount}
-            />
-          ))}
-        </div>
+        <Carousel />
       </div>
     </>
   );
--- apps/client/src/components/product-card.tsx
+++ apps/client/src/components/product-card.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// import FavoriteIcon from "@mui/icons-material/Favorite";
-// import StarRoundedIcon from "@mui/icons-material/StarRounded";
+import StarRating from "./star-rating";
 
 interface ProductCardProps {
   imageUrl: string;
   name: string;
   originalPrice: number;
-  discountRate: number | undefined;
+  discountRate?: number; // discountRate can be optional
   rating: number;
   reviewCount: number;
 }
@@ -20,77 +18,40 @@ const ProductCard: React.FC<ProductCardProps> = ({
   rating,
   reviewCount,
 }) => {
-  // Calculate the width of the filled stars based on the rating
-  const filledStarsWidth = (rating / 5) * 100;
-
   return (
-    // <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
-    //     <div className="relative">
-    //         <img className="rounded-t-lg p-8" src={imageUrl} alt={name} />
-    //         <div className="absolute top-0 right-0 p-2">
-    //             <button className="text-gray-500 hover:text-gray-700">
-    //                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    //                     <ShoppingCartIcon />
-    //                 </svg>
-    //             </button>
-    //             <button className="text-gray-500 hover:text-gray-700 ml-2">
-    //                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    //                     <FavoriteIcon />
-    //                 </svg>
-    //             </button>
-    //         </div>
-    //     </div>
-    //     <div className="p-5 bg">
-    //         <h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
-    //         <p className="text-gray-600 text-sm line-through">${originalPrice}</p>
-    //         <p className="text-gray-900 text-lg font-semibold">${currentPrice}</p>
-    //         <div className="flex items-center mt-2.5 mb-5">
-    //             <div className="flex items-center">
-    //                 <div className="relative w-full bg-gray-200 rounded h-1.5">
-    //                     <div className="absolute top-0 h-1.5 rounded" style={{ width: `${filledStarsWidth}%` }}><StarRoundedIcon className='rounded-lg fill-yellow-500 overflow-hidden' /></div>
-    //                 </div>
-    //             </div>
-    //             <span className="text-gray-600 text-sm ml-3">({reviewCount})</span>
-    //         </div>
-    //     </div>
-    // </div>
-    <div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+    <div className="group card w-96 bg-base-100 shadow-xl rounded-lg">
       <figure className="relative">
         {discountRate && (
           <div className="badge border-none bg-red-500 z-20 absolute top-4 left-4">
             <span className="text-gray-200 font-semibold">-{discountRate}%</span>
           </div>
         )}
-        <img src={imageUrl} alt="Shoes" />
+        <img src={imageUrl} alt="Product" />
+        <div className="add-to-cart-button absolute bottom-0 left-0 right-0 justify-center z-10 hidden group-hover:flex">
+          <button className="w-full text-white bg-black py-2 rounded-t-lg flex items-center justify-center text-md font-bold">
+            Add to Cart
+          </button>
+        </div>
       </figure>
-      <div className="card-body rounded-lg" data-theme="nord">
+      <div className="card-body rounded-b-lg">
         <h2 className="card-title">
           {name}
           <div className="badge badge-secondary">NEW</div>
         </h2>
         <p>
-          {
-            discountRate &&
+          {discountRate && (
             <span className="text-lg font-bold text-red-500 mr-3">
-              ${Math.floor(originalPrice*discountRate/100)}
+              ${Math.floor(originalPrice - (originalPrice * discountRate / 100))}
             </span>
-          }
-          <span className={`text-gray-600 text-lg ${ discountRate ? "line-through" : ""} start-1`}>
+          )}
+          <span className={`text-gray-600 text-lg ${discountRate ? "line-through" : ""}`}>
             ${originalPrice}
           </span>
         </p>
-        <div className="flex items-center">
-          <div className="relative w-full bg-gray-200 rounded h-1.5">
-            <div
-              className="absolute top-0 h-1.5 rounded"
-              style={{ width: `${filledStarsWidth}%` }}
-            ></div>
-          </div>
+        <div className="flex items-center mb-4 p-0">
+          <StarRating rating={rating} />
+          <span className="text-sm text-gray-500 ml-2">({reviewCount})</span>
         </div>
-        {/* <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
-        </div> */}
       </div>
     </div>
   );
