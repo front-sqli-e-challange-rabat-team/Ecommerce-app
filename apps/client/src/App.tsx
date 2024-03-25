0ee1319cda1d6b69608680a9fe2787ee0bbad59f
abelqasm <abderrahmane.belqasmi@gmail.com>
2024-03-25 10:25:59 +0000

    enhance: setup layout in app component and router children

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,15 +1,35 @@
-import { TitleUi } from '@repo/ui';
-// import Cart from './components/cart-checkout/Cart';
-import Checkout from './components/cart-checkout/Checkout';
+import { BiRightArrow } from "react-icons/bi";
+import { useAppSelector } from "./hooks/redux";
+import { Outlet } from "react-router-dom";
 
-function App() {
+import Navbar from "./components/Layouts/Navbar";
+import Footer from "./components/Layouts/Footer";
+
+const App = () => {
+  const { theme } = useAppSelector((state) => state.general);
+
   return (
-    <>
-      <div className='bg-red-300 text-center'>anas jaidi</div>
-      <TitleUi />
-      <Checkout />
-    </>
+    <div className="h-dvh w-full flex flex-col" data-theme={theme}>
+      <div
+        data-theme={theme === "dark" ? "nord" : "dark"}
+        className="w-full bg-base-100 text-base-content flex justify-center py-2 px-20 border-b-2"
+      >
+        <p className="ml-auto flex justify-center items-center">
+          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          <BiRightArrow className="mx-2" />
+          <a className="font-bold hover:underline" href="">
+            ShopNow
+          </a>
+        </p>
+        <p className="justify-self-end ml-auto">English</p>
+      </div>
+      <Navbar />
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
--- apps/client/src/routes.tsx
+++ apps/client/src/routes.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Cart from "./components/cart-checkout/Cart";
+import Checkout from "./components/cart-checkout/Checkout";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/checkout",
+        element: <Checkout />,
+      },
+    ],
   },
 ]);
 
-export default routes
+export default routes;
