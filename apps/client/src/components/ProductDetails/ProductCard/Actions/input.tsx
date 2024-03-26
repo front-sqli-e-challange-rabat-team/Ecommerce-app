d40da8045022b3e33b08a9ba9a9f7faf38e90288
Elkarni Bachar <diamobachar06@gmail.com>
2024-03-26 09:34:38 +0000

    (ProductDetails page): refactored the images showcase component, created the base logic for the quantity input

--- apps/client/src/components/ProductDetails/ProductCard/Actions/input.tsx
+++ apps/client/src/components/ProductDetails/ProductCard/Actions/input.tsx
@@ -2,9 +2,11 @@ import { TiPlusOutline } from "react-icons/ti";
 import { TiMinusOutline } from "react-icons/ti";
 import { twMerge } from "tailwind-merge";
 import { ThemeState } from "../../../../types/Theme";
+import { useState } from "react";
 
 
 const Input = ({theme}:ThemeState) => {
+  const [quantity, setQuantity] = useState<number>(0)
   return (
     <div className="flex">
       <button
@@ -12,6 +14,7 @@ const Input = ({theme}:ThemeState) => {
           "btn btn-outline  font-bold rounded-r-none text-xl w-5 !min-h-0 !h-10",
           theme == "dark" ? "btn-accent" : "btn-primary"
         )}
+        onClick={()=>setQuantity(prev=>prev>0 ? prev-1 : 0)}
       >
         <p className="bg-transparent">
           <TiMinusOutline className="bg-transparent" />
@@ -25,13 +28,18 @@ const Input = ({theme}:ThemeState) => {
             ? "input-accent text-accent "
             : "input-primary text-primary "
         )}
-        defaultValue={0}
+        value={quantity}
+        onChange={(e)=>{
+          const value = parseInt(e.target.value)
+          setQuantity(e.target.value==""?0:!isNaN(value) && value>0 ? value:0)
+        }}
       />
       <button
         className={twMerge(
           "btn btn-outline  font-bold rounded-l-none text-xl w-5 !min-h-0 !h-10",
           theme == "dark" ? "btn-accent" : "btn-primary"
         )}
+        onClick={()=>setQuantity(prev=> prev+1)} //todo: check against the available stock
       >
         <p className="bg-transparent">
           <TiPlusOutline className="bg-transparent" />
--- apps/client/src/components/ProductDetails/showcaseElements/Banner.tsx
+++ apps/client/src/components/ProductDetails/showcaseElements/Banner.tsx
@@ -2,10 +2,11 @@ import { twMerge } from "tailwind-merge";
 import { ThemeState } from "../../../types/Theme";
 
 type Props = ThemeState & {
-    url:string
+    url:string,
+    onClick: (imgUrl: string)=>void
 }
 
-const Banner = ({theme, url}:Props) => {
+const Banner = ({theme, url, onClick}:Props) => {
     return ( 
         <img
           src={url}
@@ -13,6 +14,7 @@ const Banner = ({theme, url}:Props) => {
             "h-32 object-cover border-2 rounded-lg shadow-lg",
             theme == "dark" ? "border-white" : "border-base-300"
           )}
+          onClick={()=>onClick(theme)}
         />
      );
 }
